851756a69835ef796fe0682ecc60df6f400afea8
largemable <mable.palombo@gmail.com>
2021-02-20 19:53:14 -0600

    user can click to the next card, click to reveal the dog breed name

--- script.js
+++ script.js
@@ -1,18 +1,30 @@
 // console.log('hello world')
-let dogApi = 'https://api.thedogapi.com/v1/images/search';
+let dogApi = 'https://api.thedogapi.com/v1/breeds';
+// let dogApi = 'https://api.thedogapi.com/v1/images/search';
 let dogImage = document.querySelector('#dog-image');
+let button = document.querySelector('button');
+let p = document.querySelector('p');
 
 dogImage.src = 'dogtest.jpg';
 
-dogImage.addEventListener('click', (event) => {
+button.addEventListener('click', (event) => {
+	p.innerText = '';
 	event.preventDefault();
 	console.log('you clicked doggy');
 	fetch(dogApi, {})
 		.then((res) => res.json())
 		.then((res) => {
-			console.log(res);
+			let i = Math.ceil(Math.random() * 172);
+			console.log(res[i]);
+			let doge = res[i];
+			dogImage.src = doge.image.url;
+			dogImage.addEventListener('click', (event) => {
+				// event.preventDefault();
+				p.innerText = doge.name;
+				//clear innerText when next card is clicked again
+			});
+			// console.log(doge.breeds.name.length);
 		})
 		.catch((err) => console.log(err));
 	// dogImage.src = res.url;
 });
-// grab image from api res
