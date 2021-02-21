7b1e8c43b9bc168d2266700d11521431f8d2c44b
largemable <mable.palombo@gmail.com>
2021-02-21 17:21:43 -0600

    added media query

--- script.js
+++ script.js
@@ -5,11 +5,12 @@ let dogImage = document.querySelector('#dog-image');
 let button = document.querySelector('a');
 let p = document.querySelector('p');
 
-dogImage.src = 'coooooooldog.jpg';
+// dogImage.src = 'coooooooldog.jpg';
 
 // generates a random dog
 
 button.addEventListener('click', (event) => {
+	button.innerText = 'next';
 	p.innerText = '';
 	event.preventDefault();
 	console.log('you clicked doggy');
@@ -21,7 +22,7 @@ button.addEventListener('click', (event) => {
 			let doge = res[i];
 			dogImage.src = doge.image.url;
 			dogImage.addEventListener('click', (event) => {
-				// event.preventDefault();
+				event.preventDefault();
 				p.innerText = doge.name;
 				// dogImage.innerHTML = doge.name;
 			});
@@ -31,15 +32,41 @@ button.addEventListener('click', (event) => {
 	// dogImage.src = res.url;
 });
 
-document.body.addEventListener('keydown', checkKeyPress, false);
-function checkKeyPress(key) {
-	if (key.keycode == '97') {
-		console.log('hello from a');
+document.addEventListener('keydown', (event) => {
+	p.innerText = '';
+	event.preventDefault();
+	console.log('you clicked doggy');
+	if (event.key === 'ArrowRight') {
+		fetch(dogApi, {})
+			.then((res) => res.json())
+			.then((res) => {
+				let i = Math.ceil(Math.random() * 172);
+				console.log(res[i]);
+				let doge = res[i];
+				dogImage.src = doge.image.url;
+				document.addEventListener('keydown', (event) => {
+					event.preventDefault();
+					if (event.key === 'ArrowLeft') {
+						p.innerText = doge.name;
+					}
+				});
+				// console.log(doge.breeds.name.length);
+			})
+			.catch((err) => console.log(err));
 	}
-}
+	// dogImage.src = res.url;
+});
 
 console.log('is this thing on?');
 
 // generate a deck of ordered cards user can go back and forth between
 // "save" cards or just remove the cards that user guesses right?
 // multiple choice!
+
+document.addEventListener('keydown', function onPress(event) {
+	if (event.key === 'ArrowRight') {
+		console.log('right');
+	} else if (event.key === 'ArrowLeft') {
+		console.log('left');
+	}
+});
