5a6b8db1fa92de2e35a9c421f8172978feae236e
largemable <mable.palombo@gmail.com>
2021-02-24 20:38:44 -0600

    finished... unless

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
-// console.log('hello world')
+
 let dogApi = 'https://api.thedogapi.com/v1/breeds';
-// generates a random dog
 let dogImage = document.querySelector('#dog-image');
 let button = document.querySelector('#start');
 let p = document.querySelector('#instructions');
@@ -9,7 +8,6 @@ let backButton = document.querySelector('#back');
 backButton.style.display = 'none';
 
 const displayDog = () => {
-	// dogImage.src = currentDog.image.url;
 	p.innerText = currentDog.name;
 	p.style.color = 'yellow';
 	p.style.textShadow = '-1px 0 black, 0 1px black, 1px 0 black, 0 -1px black';
@@ -19,20 +17,17 @@ button.addEventListener('click', (event) => {
 	event.preventDefault();
 	button.innerText = 'next';
 	backButton.style.display = 'block';
-	p.innerText = 'Click image to reveal breed!';
+	p.innerText = 'Tap the image to reveal the breed!';
 	p.style.color = 'black';
 	p.style.textShadow = 'none';
 	console.log('Clicked on button');
 	fetch(dogApi, {})
 		.then((res) => res.json())
 		.then((res) => {
-			let i = Math.ceil(Math.random() * 172);
-			// console.log(res[i]);
+			let i = Math.ceil(Math.random() * 172);	
 			let doge = res[i];
 			dogArray.push(doge);
-			// console.log(dogArray);
 			currentDog = dogArray[dogArray.length - 1];
-			// console.log(currentDog);
 			dogImage.src = currentDog.image.url;
 			dogImage.addEventListener('click', (event) => {
 				event.preventDefault();
@@ -57,7 +52,6 @@ backButton.addEventListener('click', (event) => {
 
 document.addEventListener('keydown', (event) => {
 	event.preventDefault();
-	// console.log('you clicked doggy');
 	if (event.key === 'ArrowLeft') {
 		event.preventDefault;
 		currentDog = dogArray[dogArray.indexOf(currentDog) - 1];
@@ -65,15 +59,13 @@ document.addEventListener('keydown', (event) => {
 		displayDog();
 	}
 	if (event.key === 'ArrowRight') {
-		// console.log('Clicked right arrow');
-		p.innerText = 'Click image to reveal breed!';
+		p.innerText = 'Tap the image to reveal the breed!';
 		p.style.color = 'black';
 		p.style.textShadow = 'none';
 		fetch(dogApi, {})
 			.then((res) => res.json())
 			.then((res) => {
 				let i = Math.ceil(Math.random() * 172);
-				// console.log(res[i]);
 				let doge = res[i];
 				dogArray.push(doge);
 				console.log(dogArray);
@@ -93,21 +85,3 @@ document.addEventListener('keydown', (event) => {
 			.catch((err) => console.log(err));
 	}
 });
-
-// generate a deck of ordered cards user can go back and forth between
-// "save" cards or just remove the cards that user guesses right?
-// multiple choice?
-// 'loading' animation
-
-// document.addEventListener('keydown', function onPress(event) {
-// 	if (event.key === 'ArrowRight') {
-// 		console.log('right');
-// 	} else if (event.key === 'ArrowLeft') {
-// 		console.log('left');
-// 	}
-// });
-
-// create an array
-// when you click start, or click to next dog, it will push the response to the array
-// when you hit start, back button appears
-// back/left arrow key will access the previous element in the array
